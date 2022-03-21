190037b26af0151ac9fc7b28899546921086a0f4
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2022-03-21 21:55:33 +0600

    added appointment form

--- src/components/pages/AppointmentForm/AppointmentForm.js
+++ src/components/pages/AppointmentForm/AppointmentForm.js
@@ -1,30 +1,119 @@
 import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
+import "./AppointmentForm.css";
 
 const AppointmentForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
       <Row xs={1} md={2} className="g-4 container mx-auto py-5">
         <Col className="col-sm-8">
-          <Card className="h-100 border-0 shadow">
-            <Card.Img variant="top" src="holder.js/100px160" />
+          <Card className="h-100 border-0 ">
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
+              <Card.Title>
+                <h3 className="appointment_content">Book an appointment</h3>
+              </Card.Title>
               <Card.Text>
-                This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                <p className="text-muted my-3 pb-3">
+                  Porta semper lacus cursus, feugiat primis ultrice in ligula risus auctor tempus feugiat dolor lacinia cubilia curae integer congue
+                  leo metus, primis in orci integer metus mollis faucibus enim. Nemo ipsam egestas volute turpis dolores ut aliquam quaerat sodales
+                  sapien undo pretium purus feugiat dolor impedit magna purus pretium gravida donec ligula massa gravida donec pretium
+                </p>
+              </Card.Text>
+              <form onSubmit={handleSubmit} action="#">
+                <input placeholder="Department" className="form-control my-2 py-2 px-3" type="text" name="department" />
+
+                <input placeholder="Appointment Date" className="form-control my-3 py-3 px-3" type="date" name="date" />
+                <input placeholder="Enter Your Name*" required className="form-control my-3 py-3 px-3" type="text" name="userName" />
+                <input placeholder="Enter Your Email*" required className="form-control my-3 py-3 px-3" type="email" name="email" />
+                <input placeholder="Enter Your Phone Number*" required className="form-control my-3 py-3 px-3" type="number" name="phone" />
+                <textarea
+                  rows="5"
+                  placeholder="Tell me your problem here..."
+                  required
+                  className="form-control my-3 py-3 px-3"
+                  type="text"
+                  name="desc"
+                />
+                <input className=" w-100 border-0 my-2 py-2 px-3 button2" type="submit" value="REQUEST AN APPOINTMENT" />
+              </form>
+              <Card.Text className="text-muted mt-5">
+                <small>
+                  * Porta semper lacus cursus, feugiat primis ultrice in ligula risus auctor tempus feugiat dolor lacinia cubilia curae integer congue
+                  leo metus, primis in orci integer metus mollis faucibus enim. Nemo ipsam egestas volute turpis dolores ut aliquam quaerat sodales
+                  sapien undo pretium purus
+                </small>
               </Card.Text>
             </Card.Body>
           </Card>
         </Col>
         <Col className="col-sm-4">
-          <Card className="h-100 shadow border-0">
-            <Card.Img variant="top" src="holder.js/100px160" />
+          <Card className="h-100  border-0">
             <Card.Body>
-              <Card.Title>Card title</Card.Title>
+              <Card.Title>The Heart Of Clinic</Card.Title>
+              <hr />
               <Card.Text>
-                This is a longer card with supporting text below as a Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate vel
-                laboriosam nemo libero rem quidem at non commodi ducimus voluptates ab fuga ipsam similique reprehenderit, adipisci saepe esse!
-                Facere, rerum!
+                <div className="d-flex justify-content-center align-items-center">
+                  <div>
+                    <img className="img-fluid w-75" src="https://i.ibb.co/hXbgbmf/head-of-clinic.jpg" alt="" />
+                  </div>
+                  <div className="doctor_info">
+                    <h5>Dr. Jonathan Barnes</h5>
+                    <p className="text-muted">
+                      <small>Chief Medical Officer, Founder</small>
+                    </p>
+                    <h6 className="color_custom fw-bold">1-800-1234-567</h6>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <p className="pb-3">
+                    <small>
+                      An enim nullam tempor sapien at gravida donec pretium ipsum porta justo integer at odiovelna vitae auctor integer congue magna
+                      purus
+                    </small>
+                  </p>
+                  <button className=" border-0 py-2 px-3 button2">About More</button>
+                </div>
+                <div className="working_time mt-5">
+                  <h5>Working Hour</h5>
+                  <p className="">
+                    Porta semper lacus cursus, feugiat primis ultrice ligula risus auctor at tempus feugiat dolor lacinia cursus nulla vitae massa
+                  </p>
+                  <div className="d-flex justify-content-between time_table">
+                    <div>
+                      <p>Mon – Wed</p>
+                    </div>
+
+                    <div>9:00 AM - 7:00 PM</div>
+                  </div>
+                  <div className="d-flex justify-content-between time_table">
+                    <div>
+                      <p>Thursday</p>
+                    </div>
+
+                    <div>9:00 AM - 6:30 PM</div>
+                  </div>
+                  <div className="d-flex justify-content-between time_table">
+                    <div>
+                      <p>Friday</p>
+                    </div>
+
+                    <div>9:00 AM - 6:00 PM</div>
+                  </div>
+                  <div className="d-flex justify-content-between time_table_last">
+                    <div>
+                      <p>Sun - Sun</p>
+                    </div>
+
+                    <div>CLOSED</div>
+                  </div>
+                  <h4>Need a personal health plan?</h4>
+                  <p>
+                    <small>Porta semper lacus cursus, and feugiat primis ultrice ligula at risus auctor</small>
+                  </p>
+                </div>
               </Card.Text>
             </Card.Body>
           </Card>
